420eee0087d342b34c1b793900e88ccbd64325c1
Roland <1124997@hr.nl>
2026-04-03 00:09:04 +0200

    feat: refactor GridPattern.vue props and add mockup.vue component

--- app/components/utils/GridPattern.vue
+++ app/components/utils/GridPattern.vue
@@ -1,42 +1,56 @@
 <script lang="ts" setup>
-	import type { HTMLAttributes } from "vue";
 	
-	interface InteractiveGridPatternProps {
-		className?: HTMLAttributes["class"];
-		squaresClassName?: HTMLAttributes["class"];
-		width?: number;
-		height?: number;
-		squares?: [number, number];
-	}
-
-	const props = withDefaults(defineProps<InteractiveGridPatternProps>(), {
-		width: 80,
-		height: 80,
-		squares: () => [80, 80],
+	const { squares, width, height, squaresClassName, className } = defineProps({
+		className: {
+			type: String,
+			default: "",
+		},
+		squaresClassName: {
+			type: String,
+			default: "",
+		},
+		width: {
+			type: Number,
+			default: 80,
+		},
+		height: {
+			type: Number,
+			default: 80,
+		},
+		squares: {
+			type: Array as unknown as () => [number, number],
+			default: () => [80, 80],
+		},
 	});
 
-	const horizontal = computed(() => props.squares[0]);
-	const vertical = computed(() => props.squares[1]);
+	// const props = withDefaults(defineProps<InteractiveGridPatternProps>(), {
+	// 	width: 80,
+	// 	height: 80,
+	// 	squares: () => [80, 80],
+	// });
+
+	const horizontal = computed(() => squares[0]);
+	const vertical = computed(() => squares[1]);
 
 	const totalSquares = computed(() => horizontal.value * vertical.value);
 
 	const hoveredSquare = ref<number | null>(null);
 
-	const gridWidth = computed(() => props.width * horizontal.value);
-	const gridHeight = computed(() => props.height * vertical.value);
+	const gridWidth = computed(() => width * horizontal.value);
+	const gridHeight = computed(() => height * vertical.value);
 
 	function getX(index: number) {
-		return (index % horizontal.value) * props.width;
+		return (index % horizontal.value) * width;
 	}
 
 	function getY(index: number) {
-		return Math.floor(index / horizontal.value) * props.height;
+		return Math.floor(index / horizontal.value) * height;
 	}
 
-	const svgClass = computed(() => ["absolute z-0 inset-0 h-screen w-screen border border-gray-100", props.className]);
+	const svgClass = computed(() => ["absolute z-0 inset-0 h-screen w-screen border border-gray-100", className]);
 
 	function getRectClass(index: number) {
-		return ["stroke-gray-100 transition-all z-0 duration-100 ease-in-out not-[&:hover]:duration-1000", hoveredSquare.value === index ? "fill-transparent" : "fill-transparent", props.squaresClassName];
+		return ["stroke-gray-100 transition-all z-0 duration-100 ease-in-out not-[&:hover]:duration-1000", hoveredSquare.value === index ? "fill-transparent" : "fill-transparent", squaresClassName];
 	}
 
 	function handleMouseEnter(index: number) {
