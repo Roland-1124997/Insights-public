0ae4a6bd62aab2cef59342f03363ee99bb12c72c
Roland <1124997@hr.nl>
2026-04-03 00:29:30 +0200

    feat: add status API endpoint with asynchronous response

---
+++ server/api/status.get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+            return resolve({ "status": 200, "message": "OK" })
+        }, 200)
+    })
+})
