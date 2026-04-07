a56aa2a16be1fadbc85cbd0d166de25bc0d14cbb
Roland <1124997@hr.nl>
2026-04-07 18:06:06 +0200

    fix: update image margin in tiptap styles for better spacing

--- app/pages/[slug]/index.vue
+++ app/pages/[slug]/index.vue
@@ -2,7 +2,8 @@
 	<div class="">
 		<section class="z-20 md:pl-[4.7rem] md:pr-[3.5rem] md:p-3">
 			<article class="relative xl:-mt-5 md:p-3">
-				<div id="tiptap-container" v-if="editor" class="grid w-full gap-3 overflow-y-scroll md:h-[86.6vh] md:grid-cols-[1.009fr_0.45fr]">
+				<div ref="tiptap_container" v-if="editor" class="grid w-full gap-3 overflow-y-scroll md:h-[86.6vh] md:grid-cols-[1.009fr_0.45fr]">
+
 					<div class="px-6 pt-8 bg-white md:px-24 md:pt-10">
 						<TiptapEditor :editor="editor" aria-label="Artkel inhoud" />
 					</div>
@@ -25,6 +26,8 @@
 	const article = useArtcles();
 	const result = article.getBySlug(slug);
 
+	const tiptapContainer = useTemplateRef("tiptap_container");
+	
 	const seoTitle = `Insights - ${result?.title ?? "Artikel"}`;
 	const seoDescription = result?.description.slice(0, 155) ?? "Lees dit artikel.";
 	const seoImage = result?.thumbnail_url ?? "/icons/icon_512-blue.png";
@@ -92,9 +95,10 @@
 	let viewportMediaQuery: MediaQueryList | null = null;
 
 	const getScrollParent = () => {
+
 		if (isMobileViewport.value) return window;
 
-		const container = document.getElementById("tiptap-container");
+		const container = tiptapContainer.value
 		return container ?? window;
 	};
 
